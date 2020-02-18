16874a49518c49aec8866c57fe3ea2e06bec6e24
Anusha Valasapalli <anushava.in@mouritech.com>
2020-02-18 18:09:08 +0530

    Initial Commit

---
+++ MFS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MFS
+//
+//  Created by Mouritech on 18/02/20.
+//  Copyright © 2020 Mouritech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
